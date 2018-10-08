e311ff774352af6c8fee1fe36ab931e202094d0f
tlhpaul <tsehsu@seas.upenn.edu>
2018-10-08 13:12:07 -0400

    created tensorflow file

---
+++ CIS537_tensorflow.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 08 13:11:15 2018
+
+@author: Nehal Doiphode, Tse-Lun Hsu, Michael Zietz
+"""
+
+import os
+import sys
+import timeit
+import cPickle
+import numpy as np
+import tensorflow as tf
+
+
+if __name__ == '__main__':
+    evaluate_lenet5()
