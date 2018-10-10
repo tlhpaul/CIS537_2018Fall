e4863340294025a2a21dca09677b7f98f4084391
tlhpaul <tsehsu@seas.upenn.edu>
2018-10-10 09:05:47 -0400

    update readme

--- CIS537_tensorflow.py
+++ CIS537_tensorflow.py
@@ -8,10 +8,51 @@ Created on Mon Oct 08 13:11:15 2018
 import os
 import sys
 import timeit
-import cPickle
+import _pickle as cPickle
 import numpy as np
 import tensorflow as tf
+# import yaml
+
+
+class LeNetConvPoolLayer(object):
+    def __init__(self,rng,input,filter_shape,image_shape,poolsize=(2,2)):
+        
+        assert image_shape[1] == filter_shape[1]
+ 
+        self.input=input
+        fan_in=np.prod(filter_shape[1:])
+        fan_out = (filter_shape[0]*np.prod(filter_shape[2:])/np.prod(poolsize))
+        W_bound=np.sqrt(6./(fan_in + fan_out))
+        self.W= theano.shared(
+            np.asarray(rng.uniform(low=-W_bound,high=W_bound,size=filter_shape),
+                       dtype=theano.config.floatX),borrow=True)
+        
+        b_values=np.zeros((filter_shape[0],),dtype=theano.config.floatX)
+        self.b=theano.shared(value=b_values,borrow=True)
+        
+        #convolve input feature maps with filters            
+        conv_out=conv.conv2d(
+            input=input,
+            filters=self.W,
+            filter_shape=filter_shape,
+            image_shape=image_shape)
+
+        #downsample each feature map individually via max pooling        
+        pooled_out=downsample.max_pool_2d(
+            input=conv_out,
+            ds=poolsize,
+            ignore_border=True)
+            
+        self.output=T.tanh(pooled_out + self.b.dimshuffle('x',0,'x','x'))
+        self.params = [self.W,self.b]
+        self.input = input               
+
 
 
 if __name__ == '__main__':
-    evaluate_lenet5()
+	print ("hi")
+    # with open("environment.yml", 'r') as stream:
+	   #  try:
+	   #      print(yaml.load(stream))
+	   #  except yaml.YAMLError as exc:
+	   #      print(exc)
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # CIS537_2018Fall
 CIS 537 Biomedical Image Analysis - Gastounioti/Kontos Project
+
+## Set up conda environment 
+We use conda yaml file to configure the enviornment. You can find
+the shell file here: https://conda.io/miniconda.html
+
+### Install
+After running shell file, do the following
+anaconda 2 :
+```
+export PATH=~/anaconda2/bin:$PATH
+```
+anaconda 3 :
+```
+export PATH=~/anaconda3/bin:$PATH
+```
+Then you should be able to do 
+```
+conda env create -f environment.yml
+```
+```
+conda activate cis537
+```
+Then when running python files, they will be in this envrionment.
