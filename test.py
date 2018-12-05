b9fa10052367caa52377c54f1c4356ce6e84cc9f
tlhpaul <tsehsu@seas.upenn.edu>
2018-12-05 13:39:10 -0500

    update rename

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basepath = '/Users/paulhsu/CIS537_Data'
-for fname in os.listdir(basepath):
-    path = os.path.join(basepath, fname)
-    print (path)
-    if os.path.isdir(path):
-        continue
